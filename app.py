e47c57a6392bdc37263dbd0c8ff55dee20ed78f6
재우 <jaewoo98@kakao.com>
2023-06-02 15:22:24 +0900

    fix

--- app.py
+++ app.py
@@ -1,12 +1,13 @@
 from flask import Flask, render_template
 
+
 app = Flask(__name__)
+
 
 
 @app.route('/')
 def main_page():
     return render_template('main.html')
-
 
 @app.route('/login')
 def login_page():
