5bc822cb1f3c10d8a0391668f2a795e766cd90d1
재우 <jaewoo98@kakao.com>
2023-06-09 18:55:29 +0900

    add modal

---
+++ static/js/modal.js
@@ -0,0 +1,27 @@
+function showModal(modalId) {
+  var modal = document.getElementById(modalId);
+  modal.style.display = "block";
+}
+
+function hideModal(modalId) {
+  var modal = document.getElementById(modalId);
+  modal.style.display = "none";
+}
+
+document.addEventListener("DOMContentLoaded", function () {
+  var modalTriggers = document.querySelectorAll("[data-toggle='modal']");
+  modalTriggers.forEach(function (trigger) {
+    var target = trigger.getAttribute("data-target");
+    trigger.addEventListener("click", function () {
+      showModal(target);
+    });
+  });
+
+  var closeButtons = document.querySelectorAll("[data-dismiss='modal']");
+  closeButtons.forEach(function (button) {
+    var modal = button.closest(".modal");
+    button.addEventListener("click", function () {
+      hideModal(modal.id);
+    });
+  });
+});
